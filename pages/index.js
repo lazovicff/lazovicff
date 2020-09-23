d3d7844129a2259b19f3941c458d53395b047fea
Filip <lazoviccorp@gmail.com>
2020-09-23 23:24:44 +0200

    added cv

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,13 @@ const Index = () => (
 			<a href="/presentation.pdf">presentation</a>
 		</div>
 		<div>
-			<a href="/zavrsni_rad.pdf">thesis</a>
+			<a href="/zavrsni_rad.pdf">zavrsni_rad</a>
+		</div>
+		<div>
+			<a href="/thesis.pdf">thesis</a>
 		</div>
 		<div>
 			<a href="/demo">demo</a>
-		</div>
-		<div>
-			<a href="https://docs.google.com/presentation/d/12Kp-QO-GQHIYzxn8yBJB2Tn6t0atiaqJv3-92Gs5WKo/edit?usp=sharing">presentation_google_slides</a>
-		</div>
-		<div>
-			<a href="https://docs.google.com/document/d/1jhZNdtsqbmzNgTIgvB9NV-ruRiVJho1enbe_v-MEPKU/edit?usp=sharing">thesis_google_docs</a>
 		</div>
 	</div>
 );
